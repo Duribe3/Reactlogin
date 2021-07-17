a7cf975c7545e9ceb21b285ddd260d7ec20b5f90
Uribe <danieluribe3@hotmail.com>
2021-07-17 16:07:02 -0500

    COMIENZO DE LOGIN

---
+++ src/pages/login/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Title from './components/title/title';
+import Label from './components/label/Label';
+
+const Login =()=>{
+return (
+
+    <div>
+<Title text ='Daniel Uribe Mazo'/>
+<Label text='Usuario'/>
+<Label text='Contraseña'/>
+    </div>
+)
+
+
+};
+
+
+export default Login;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/pages/login/components/label/Label.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Label =({text}) => {
+return (
+
+<div>
+
+
+<label> {text}  </label>
+
+
+</div>
+)
+
+
+}
+
+export default Label;
